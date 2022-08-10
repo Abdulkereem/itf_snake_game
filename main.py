f998a89e3d56b9d4ec9e99b32a2c58495bc436b4
Abdulkereem <abdulkereemokereem@gmail.com>
2022-08-10 13:23:03 +0100

    table model

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy.orm import declarative_base
+
+db  = declarative_base()
+
+class User(db):
+    __tabelname__ = "user_account"
+
+    id =  Column(Integer,primary_key=True)
+    fullname = Column(String(255))
+    gender  = Column(String(30))
