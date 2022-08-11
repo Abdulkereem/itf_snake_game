13502d72b65bd9e2ff1614d2a880668cedde204c
Abdulkereem <abdulkereemokereem@gmail.com>
2022-08-11 12:37:50 +0100

    databas file added

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import Integer
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine 
+
+engine = create_engine('sqlite:///test.db')
 
 db  = declarative_base()
 
